fc27d35432a120c52394a8c705837d87a0db3e06
Sandip Torane <sandip.torane@perennialsys.com>
2021-02-12 15:27:48 +0530

    added comments

--- bookstore/server/main.go
+++ bookstore/server/main.go
@@ -6,8 +6,8 @@ import (
 	"google.golang.org/grpc"
 	"log"
 	"net"
-
 )
+
 func main() {
 	fmt.Println("gRPC bookService server running!")
 	listen, err := net.Listen("tcp", ":8082")
@@ -15,30 +15,31 @@ func main() {
 		log.Fatal("Listen err on port 8082 :", err)
 	}
 	s := Server{}
-	s.initializeServer()  //add some books to server
+
+	s.initializeServer() //add some books to server
 
 	//initialize new grpc server
 	grpcServer := grpc.NewServer()
-	books.RegisterBookServiceServer(grpcServer,&s)
-	if err:= grpcServer.Serve(listen); err!=nil{
-		log.Fatal("failed to serve:",err)
+	books.RegisterBookServiceServer(grpcServer, &s)
+	if err := grpcServer.Serve(listen); err != nil {
+		log.Fatal("failed to serve:", err)
 	}
 }
 
-func (s *Server)initializeServer(){
+func (s *Server) initializeServer() {
 	book1 := &books.Book{
 		Id:      1,
 		Title:   "abc",
 		Author:  "xyz",
 		Content: "sggr",
 	}
-	book2 :=&books.Book{
+	book2 := &books.Book{
 		Id:      2,
 		Title:   "dfg",
 		Author:  "me",
 		Content: "something",
 	}
 	var bookStore []*books.Book
-	bookStore = append(bookStore,book1,book2)
+	bookStore = append(bookStore, book1, book2)
 	s.bookStore = bookStore
 }
